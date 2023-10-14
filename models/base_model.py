d8ee88620c0f11bc6612997e4f54a727ff59138a
olusiekwin <olusiekwingraham@gmail.com>
2023-10-15 00:33:20 +0300

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,72 @@
 #!/usr/bin/python3
-'''
-Module Docs:
-'''
+"""
+BaseModel - Unified Implementation
+"""
+
 from datetime import datetime
-import uuid
+from uuid import uuid4
+from models import storage
 
 
-class MyModel:
-    '''
-    Class Docs
-    '''
+class BaseModel:
+    """
+    This class defines all common attributes/methods for other classes
+    """
 
-    def __init__(self):
-        '''
-        Docs
-        '''
-        self.my_id = str(uuid.uuid4())
-        self.my_created_at = datetime.now()
-        self.my_updated_at = self.my_created_at
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes public instance attributes.
 
-    def my_save(self):
-        '''
-        Docs
-        '''
-        self.my_updated_at = datetime.now()
+        Args:
+            args: Not used
+            kwargs: A dictionary to hold key-value arguments
 
-    def to_my_dict(self):
-        '''
-        Docs
-        '''
-        my_dictionary = self.__dict__.copy()
-        my_dictionary["__class__"] = type(self).__name__
-        my_dictionary["my_created_at"] = self.my_created_at.isoformat()
-        my_dictionary["my_updated_at"] = self.my_updated_at.isoformat()
+        Public instance attributes:
+            id: Assigns a unique ID to each instance created using uuid4
+            created_at: Assigns current datetime when an instance is created
+            updated_at: current datetime when an instance is created or updated
+        """
 
-        return my_dictionary
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    def __my_str__(self):
-        '''
-        Docs
-        '''
-        class_name = type(self).__name__
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['updated_at', 'created_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'
+                                                  )
+                    setattr(self, key, value)
+        if self.id not in storage.all():
+            storage.new(self)
 
-        return "[{}] ({}) {}".format(class_name, self.my_id, self.__dict__)
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current time.
+        """
+        now = datetime.now()
+        self.updated_at = now.isoformat()
+        self.updated_at = datetime.strptime(self.updated_at,
+                                            '%Y-%m-%dT%H:%M:%S.%f')
+        storage.save()
+
+    def to_dict(self):
+        """
+        Returns a dictionary of all key-values of __dict__ of the instance.
+        """
+
+        dictionary = self.__dict__.copy()
+
+        for key, value in dictionary.items():
+            if key in ['updated_at', 'created_at']:
+                value = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+            dictionary[key] = value
+
+        return {'__class__': type(self).__name__, **dictionary}
--- test/test_models/test_base_model.py
+++ test/test_models/test_base_model.py
@@ -1,146 +1,71 @@
 #!/usr/bin/python3
-'''
-Module
-'''
+"""
+Unit Tests for BaseModel Class - Unified Implementation
+"""
+
 import unittest
-from unittest.mock import patch
-from models.base_model import MyModel
-from uuid import UUID
 from datetime import datetime
+from models.base_model import BaseModel
 
 
-class TestMyModel(unittest.TestCase):
-    '''
-    Docs
-    '''
-    def test_moduleDocs(self):
-        '''
-        Docs
-        '''
-        moduleDoc = __import__("models.base_model").base_model.__doc__
-        self.assertGreater(len(moduleDoc), 0)
+class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for the BaseModel class
+    """
 
-    def test_classDocs(self):
-        '''
-        '''
-        classDoc = __import__("models.base_model").base_model.MyModel.__doc__
-        self.assertGreater(len(classDoc), 0)
+    def test_init(self):
+        """
+        Test for the correct initialization of attributes
+        """
+        cls = BaseModel()
+        self.assertIsNotNone(cls.id)
+        self.assertIsInstance(cls.created_at, datetime)
+        self.assertIsInstance(cls.updated_at, datetime)
 
-    def test_methodDocsSave(self):
-        '''
-        '''
-        methodDoc = (
-                __import__("models.base_model")
-                .base_model.MyModel.my_save.__doc__)
-        self.assertGreater(len(methodDoc), 0)
+    def test_str(self):
+        """
+        Test for __str__ to return the correct format
+        """
+        cls = BaseModel()
+        string = str(cls)
+        self.assertIn('[BaseModel', string)
+        self.assertIn(cls.id, string)
 
-    def test_methodDocsto_dict(self):
-        '''
-        '''
-        methodDoc = (
-                __import__("models.base_model")
-                .base_model.MyModel.to_my_dict.__doc__)
-        self.assertGreater(len(methodDoc), 0)
+    def test_save(self):
+        """
+        Test for the save function to compare time on updated_at
+        """
+        cls = BaseModel()
+        last_updated_at = cls.updated_at
+        cls.save()
+        self.assertLess(last_updated_at, cls.updated_at)
 
-    def test_methodDocs__str___(self):
-        '''
-        '''
-        methodDoc = (
-                __import__("models.base_model")
-                .base_model.MyModel.__my_str__.__doc__)
-        self.assertGreater(len(methodDoc), 0)
+    def test_created_at_save(self):
+        """
+        Test for the save function to compare created_at
+        """
+        cls = BaseModel()
+        last_created_at = cls.created_at
+        cls.save()
+        self.assertEqual(last_created_at, cls.created_at)
 
-    def test_my_idType(self):
-        '''
-        '''
-        obj = MyModel()
-        self.assertIs(type(obj.my_id), str)
+    def test_id_save(self):
+        """
+        Test for save method to save the id
+        """
+        cls = BaseModel()
+        c_id = cls.id
+        cls.save()
+        self.assertEqual(c_id, cls.id)
 
-    def test_my_idLength(self):
-        '''
-        '''
-        obj = MyModel()
-        self.assertEqual(len(obj.my_id), 36)
-
-    def test_my_idValidity(self):
-        '''
-        '''
-        obj = MyModel()
-        value = UUID(obj.my_id)
-        self.assertIs(type(value), UUID)
-
-    def test_my_created_atType(self):
-        '''
-        '''
-        obj = MyModel()
-        self.assertIs(type(obj.my_created_at), datetime)
-
-    def test_my_updated_atType(self):
-        '''
-        '''
-        obj = MyModel()
-        self.assertIs(type(obj.my_updated_at), datetime)
-
-    def test_outputOf__str__(self):
-        '''
-        '''
-        obj = MyModel()
-        str1 = f"[MyModel] ({obj.my_id}) {obj.__dict__}"
-        str2 = str(obj)
-
-        self.assertEqual(str1, str2)
-
-    def test_my_updated_atChanged(self):
-        '''
-        '''
-        obj = MyModel()
-        initial_value = obj.my_updated_at
-        obj.my_save()
-
-        self.assertGreater(obj.my_updated_at, initial_value)
-
-    def test_to_my_dictCheck(self):
-        '''
-        '''
-        obj = MyModel()
-        to_dict_dict = obj.to_my_dict()
-        __dict__dict = obj.__dict__
-
-        for keys in __dict__dict:
-            self.assertIn(keys, to_dict_dict)
-
-    def test_to_my_dict(self):
-        '''
-        '''
-        obj = MyModel()
-        to_dict_dict = obj.to_my_dict()
-
-        self.assertIn("__class__", to_dict_dict)
-        self.assertIs(type(to_dict_dict["__class__"]), str)
-
-    def test_to_my_dict_Valid(self):
-        '''
-        '''
-        obj = MyModel()
-        to_dict_dict = obj.to_my_dict()
-        created_at = datetime.fromisoformat(to_dict_dict["my_created_at"])
-        updated_at = datetime.fromisoformat(to_dict_dict["my_updated_at"])
-
-        self.assertIn("my_created_at", to_dict_dict)
-        self.assertIn("my_updated_at", to_dict_dict)
-        self.assertIs(type(to_dict_dict["my_created_at"]), str)
-        self.assertIs(type(to_dict_dict["my_updated_at"]), str)
-        self.assertEqual(to_dict_dict["my_created_at"], created_at.isoformat())
-        self.assertEqual(to_dict_dict["my_updated_at"], updated_at.isoformat())
-
-    def test_dictType(self):
-        '''
-        '''
-        obj = MyModel()
-        to_dict_dict = obj.to_my_dict()
-
-        self.assertIs(type(to_dict_dict["my_id"]), str)
+    def test_to_dict(self):
+        """
+        Test for to_dict method
+        """
+        cls = BaseModel()
+        cls_dict = cls.to_dict()
+        self.assertIsInstance(cls_dict, dict)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
