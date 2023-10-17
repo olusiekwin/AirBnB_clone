c650adf860fa7bd748b55bcd0197f98f4b971250
olusiekwin <olusiekwingraham@gmail.com>
2023-10-17 16:20:46 +0300

    User_-_Account

--- models/user.py
+++ models/user.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+'''
+Module Docs:
+'''
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel."""
+    '''
+    Class Docs
+    '''
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize User instance."""
-        super().__init__(*args, **kwargs)
