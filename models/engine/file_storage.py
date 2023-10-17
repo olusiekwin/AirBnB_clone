bf3ee31f073a3d529312c7b3fb4578ec4852ed0f
olusiekwin <olusiekwingraham@gmail.com>
2023-10-17 16:57:41 +0300

    Engine_-_Files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,37 +1,63 @@
 #!/usr/bin/python3
-import json
+'''
+Module Docs
+'''
+from json import dumps, loads
+from os.path import isfile
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
+    '''
+    Class Docs
+    '''
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self, models):
-        self.models = models  # Store the class mappings
-
     def all(self):
+        '''
+        Docs
+        '''
         return FileStorage.__objects
 
     def new(self, obj):
+        '''
+        Docs
+        '''
         key = f"{obj.__class__.__name__}.{obj.id}"
+
         FileStorage.__objects[key] = obj
 
     def save(self):
-        obj_dict = {}
-        for key, value in FileStorage.__objects.items():
-            obj_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+        '''
+        Docs
+        '''
+        final_dict = {
+                key: value.to_dict() for key, value
+                in FileStorage.__objects.items()}
+        json_string = dumps(final_dict)
+        filename = FileStorage.__file_path
+        with open(filename, "w") as f:
+            f.write(json_string)
 
     def reload(self):
-        try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
-                    class_name, obj_id = key.split('.')
-                    cls = self.models[class_name]
-
-                    new_obj = cls(**value)
-                    FileStorage.__objects[key] = new_obj
-        except FileNotFoundError:
-            pass
+        '''
+        Docs
+        '''
+        allowed_classes = ["BaseModel", "User", "State", "City",
+                           "Amenity", "Place", "Review"]
+        filename = FileStorage.__file_path
+        if isfile(filename):
+            with open(filename, "r") as f:
+                json_string = f.read()
+                final_dict = loads(json_string)
+            for key, value in final_dict.items():
+                class_name, obj_id = key.split(".")
+                if class_name in allowed_classes:
+                    eval("self.new({}(**value))".format(class_name))
