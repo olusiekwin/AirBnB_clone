0d6c18afec376477ac050a19ef115eca3c08d132
olusiekwin <olusiekwingraham@gmail.com>
2023-10-17 16:41:50 +0300

    Missing-File

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+Module
+'''
+import unittest
+from models.engine.file_storage import FileStorage
+from uuid import UUID
+from datetime import datetime
+
+
+class TestFileStorage(unittest.TestCase):
+    '''
+    Docs
+    '''
+
+    def test_moduleDocs(self):
+        '''
+        Docs
+        '''
+        moduleDoc = (
+                __import__("models.engine.file_storage")
+                .engine.file_storage.__doc__)
+        self.assertGreater(len(moduleDoc), 0)
+
+    def test_classDocs(self):
+        '''
+        Docs
+        '''
+        classDoc = (
+                __import__("models.engine.file_storage")
+                .engine.file_storage.FileStorage.__doc__)
+        self.assertGreater(len(classDoc), 0)
+
+    def test_methodDocsSave(self):
+        '''
+        Docs
+        '''
+        methodDoc = (
+                __import__("models.engine.file_storage")
+                .engine.file_storage.FileStorage.save.__doc__)
+        self.assertGreater(len(methodDoc), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
