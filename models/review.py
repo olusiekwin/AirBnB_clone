784fe06f73289e01c17e1d07c1984d90cbc5e020
olusiekwin <olusiekwingraham@gmail.com>
2023-10-17 16:23:11 +0300

    Reviews

--- models/review.py
+++ models/review.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+'''
+Module Docs:
+'''
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review class that inherits from BaseModel."""
+    '''
+    Class Docs
+    '''
+
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize Review instance."""
-        super().__init__(*args, **kwargs)
